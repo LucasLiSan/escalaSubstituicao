b5e835b654b0d4629e9cdd770c05f8ecac183c6a
LucasLiSan <lukassantana_rgt@hotmail.com>
2023-05-23 12:02:10 -0300

    escala atualizada

--- _assets/_js/script.js
+++ _assets/_js/script.js
@@ -1,6 +1,13 @@
+/*
 function goToWeek() {
-    const x = document.getElementById('semana').value;
-    var semana = x.split('-');
-    var week = semana.slice(1);
-    console.log(week[0]);
+    var data = document.getElementById('semana').value;
+    var semana = data.split('-');
+    var page = semana.slice(1);
+    var pageID = "#".concat(page);
+    var link = "index.html".concat(pageID);
+    if (data != "") {
+        window.onclick.href.replace(link);
+    } 
 };
+
+document.getElementById("search").addEventListener("click", goToWeek());*/
